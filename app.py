4f8b89c069ec45a8334b705b37057283163f6802
a-842 <131918994+a-842@users.noreply.github.com>
2025-02-22 22:01:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_mail import Mail, Message
 from werkzeug.security import generate_password_hash, check_password_hash
-from werkzeug.middleware.proxy_fix import ProxyFix
 import random, os
 from dotenv import load_dotenv
 
