a40ddd331a9b0785c05c7477b16166efdf1b944c
a-842 <131918994+a-842@users.noreply.github.com>
2025-02-22 10:24:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import random, os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your-email@gmail.com'  # Replace
-app.config['MAIL_PASSWORD'] = 'your-email-password'  # Replace
 
 db = SQLAlchemy(app)
 mail = Mail(app)
